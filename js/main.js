6d5c361ed2b4e56a76d38f14c3efc0c6bd6b4fb1
Keith Ort <keithort@gmail.com>
2015-05-22 00:17:49 -0400

    mobile nav fix. updated convicted images.

--- js/main.js
+++ js/main.js
@@ -8,9 +8,8 @@ $('a.mobile, a.close').on('click', function () {
 
 // Smooth scrolling for navigation anchor links
 var $root = $('html, body'),
-mastHeadHeight = $('.masthead').outerHeight();
+  mastHeadHeight = $('.masthead').outerHeight();
 $('.nav a').on('click', function () {
-  console.log($(this));
   if ($('.nav').hasClass('active')) {
     $('.nav, a.close').removeClass('active');
   }
@@ -32,44 +31,23 @@ function sticky() {
       'padding-top' : $('.masthead').outerHeight()
     });
     $('.masthead').addClass('sticky');
-    $('.social').addClass('sticky');
   } else {
     $('body').css({
       'padding-top' : '0px'
     });
     $('.masthead').removeClass('sticky');
-    $('.social').removeClass('sticky');
   }
 }
 sticky();
 $(window).on('scroll', function () {
   sticky();
-});
-
-// Social media
-$('.socicon').on('click', function () {
-  if ($(this).hasClass('facebook')) {
-    ga('send', 'event', 'Navigation', 'Share', 'FB');
-    window.open('https://www.facebook.com/dialog/share?app_id=911788002215384&display=popup&href=https%3A%2F%2Fwww.inkofinnocence.org&redirect_uri=https%3A%2F%2Fwww.inkofinnocence.org', 'sharer', 'top=20,left=50,toolbar=0,status=0,width=626,height=305'); 
-  }
-  else if ($(this).hasClass('twitter')) {
-    ga('send', 'event', 'Navigation', 'Share', 'Twitter');
-    window.open('https://twitter.com/intent/tweet?text=Up%20to%20100,000%20people%20in%20US%20prisons%20are%20innocent.%20Put%20DNA%20that%20could%20set%20them%20free%20directly%20in%20front%20of%20lawmakers.%20https://inkofinnocence.org', 'sharer', 'top=20,left=50,toolbar=0,status=0,width=626,height=305');    
-  }
-  else if ($(this).hasClass('googleplus')) {
-    ga('send', 'event', 'Navigation', 'Share', 'Google+');
-    window.open('https://plus.google.com/share?url=https%3A//www.inkofinnocence.org', 'sharer', 'top=20,left=50,toolbar=0,status=0,width=626,height=305');    
+  $('.masthead.sticky').css('width', window.innerWidth + ('px'));
+  if(navigator.platform.indexOf('iPad') > -1 || navigator.platform.indexOf('iPhone') > -1 || navigator.platform.indexOf('iPod') > -1) {
+    $('.masthead.sticky').css('position', 'fixed'); 
   }
 });
-
-// Configuration for video lightbox
-$('.lightbox').magnificPopup({
-  disableOn: 700,
-  type: 'iframe',
-  mainClass: 'mfp-fade',
-  removalDelay: 160,
-  preloader: false,
-  fixedContentPos: false
+$(window).resize(function () {
+  $('.masthead.sticky').css('width', window.innerWidth + ('px'));
 });
 
 // Configuration for Convicts carousel
@@ -87,7 +65,6 @@ $('.carousel').slick({
 
 $('.carousel button').on('click', function (event, slick){
 	var currentSlide = $('.carousel').slick('slickCurrentSlide');
-  console.log('slide ' + currentSlide);
   changeText(currentSlide);
 });
 
@@ -96,20 +73,17 @@ var topText = [
   "They’ve taken my name. They’ve taken my family.",
   "They’ve taken my freedom. They’ve taken my dignity.",
   "They’ve taken my friends. They’ve taken my children.",
-  "They’ve taken my name. They’ve taken my family.",
+  //"They’ve taken my name. They’ve taken my family.",
   "They’ve taken my reputation. They’ve taken my credibility."
 ];
 var bottomText = [
   "They’ve taken my life.",
   "They’ve taken everything.",
   "They haven’t taken my hope.",
-  "They’ve taken everyone.",
+  //"They’ve taken everyone.",
   "They’ve taken my future."
 ];
 function changeText(currentSlide) {
-  console.log('function currentSlide ' + currentSlide);
-  console.log(topText[currentSlide]);
-  console.log(bottomText[currentSlide]);
   $('.bottom .dek').html(topText[currentSlide]);
   $('.bottom .quote').html(bottomText[currentSlide]);
   return true;
